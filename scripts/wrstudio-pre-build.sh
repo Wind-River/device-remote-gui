f290bce3d183aea50806601740be0d66d1f4ca9f
Per Hallsmark <per.hallsmark@windriver.com>
2021-09-17 13:21:51 +0200

    wr studio : lock dependant layer versions
    Signed-off-by: Per Hallsmark <per.hallsmark@windriver.com>

--- scripts/wrstudio-pre-build.sh
+++ scripts/wrstudio-pre-build.sh
@@ -12,7 +12,9 @@ set -x
 
 git clone --single-branch --branch master ${GITLAB_URL}/phallsma/device-remote-gui.git
 git clone --single-branch --branch master ${GITLAB_URL}/phallsma/meta-javascripts.git
+git checkout 9a0c97e935a8bc4e17a62baa7f619ff5aa295742
 git clone --single-branch --branch master ${GITLAB_URL}/phallsma/meta-marine-instruments.git
+git checkout 9ab86ccb5f3c7b429badcf8d9563b32c50582a01
 
 bitbake-layers add-layer -F $PWD/device-remote-gui/layers/meta-project-addons \
                             $PWD/meta-javascripts \
